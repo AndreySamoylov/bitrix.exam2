242bf670f2b09e5ff1a5f84465091a6a279148e9
Андрей Самойлов <san@d-element.ru>
2024-11-18 11:18:29 +0500

    update index.php

--- index.php
+++ index.php
@@ -2,31 +2,6 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Мебельная компания");
 $USER->Authorize(1); // авторизуем
-
-if(CModule::IncludeModule("iblock")) {
-    $arSelect = array("ID", "IBLOCK_ID", "NAME", "DATE_ACTIVE_TO");
-    $arFilter = array("IBLOCK_ID" => IntVal(STOCK_IBLOCK_ID), "<=DATE_ACTIVE_TO" => ConvertTimeStamp(false, "FULL"));
-    $res = CIBlockElement::GetList(array(), $arFilter, false, false, $arSelect);
-    $items = [];
-    while ($item = $res->Fetch()) {
-        $items[] = $item;
-    }
-
-    if (is_array($items) && count($items) > 0) {
-        $arEventFields = array(
-            "COUNT" => count($items),
-        );
-
-        CEvent::Send("WARMING_USER", "s1", $arEventFields);
-
-        CEventLog::Add(array(
-            "SEVERITY" => "SECURITY",
-            "AUDIT_TYPE_ID" => "MY_OWN_TYPE",
-            "MODULE_ID" => "main",
-            "DESCRIPTION" => "Акций с истекшей датой активности = " . count($items),
-        ));
-    }
-}
 ?>
 <?php
 $APPLICATION->IncludeComponent(
